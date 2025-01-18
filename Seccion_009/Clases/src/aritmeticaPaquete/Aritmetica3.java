de7a8f861d4230a6b8e21289f7fc3fd5300287b1
Laptop <memorosas.00@gmail.com>
2025-01-17 21:43:24 -0600

    termino, leccion 108

--- Seccion_009/Clases/src/aritmeticaPaquete/Aritmetica3.java
+++ Seccion_009/Clases/src/aritmeticaPaquete/Aritmetica3.java
@@ -29,45 +29,22 @@ public class Aritmetica3 {
     }
 
     // las variables aqui dentro se crean y destruyen cuando se ejcuta bloque, duran poco
-    void sumar (){
+    public void sumar (){
 
         var resultado = operando1 + operando2;
         System.out.println("Resultado suma: " + resultado);
 
     }
 
-    void restar (){
+    // public nos ayuda a acceder desde otro archivo al metodo
+    public void restar (){
 
         int resultado = operando1 - operando2;
         System.out.println("Resta es: " + resultado);
 
     }
 
-    public static void main(String[] args) {
-        
-        // aqui se crean 2 objetos
-        System.out.println("*** Metodo aritmetica ***");
-        // el operador this, se crea al hacer la instancia, es como un apuntador
-        // si hay una nueva instancia , el operador this cambiara
-        var Aritmetica1 = new Aritmetica3(10, 8);
-        Aritmetica1.sumar();
-        Aritmetica1.restar();
-        // lo que esta depues del @ es la memoria RAM @72ea2f77
-        System.out.println("Dir Memoria: aritmetica1: " + Aritmetica1);
-        System.out.println();
-
-        // este da error, porque espra 2 argumentos, pero haciendo en constructo vacio, ya jala
-        // sobrecarga de constructores
-        var Aritmetica2 = new Aritmetica3(15,30);
-        Aritmetica2.operando1 = 15;
-        Aritmetica2.operando2 = 35;
-
-        // aqui vemos como el oerador this cambia en cada instancia/objeto
-        System.out.println("Dir Memoria: aritmetica2: " + Aritmetica2);
-
-
-    }
-
+    
 }
 
 
--- Seccion_009/Clases/src/pruebaPaquete/PruebaAritmetica.java
+++ Seccion_009/Clases/src/pruebaPaquete/PruebaAritmetica.java
@@ -1,6 +1,35 @@
 package pruebaPaquete;
 
+import aritmeticaPaquete.Aritmetica3;
+
 public class PruebaAritmetica {
 
     // min 4:14
+
+    public static void main(String[] args) {
+        
+        // aqui se crean 2 objetos
+        System.out.println("*** Metodo aritmetica ***");
+        // el operador this, se crea al hacer la instancia, es como un apuntador
+        // si hay una nueva instancia , el operador this cambiara
+        var Aritmetica1 = new Aritmetica3(10, 8);
+        Aritmetica1.sumar();
+        Aritmetica1.restar();
+        // lo que esta depues del @ es la memoria RAM @72ea2f77
+        System.out.println("Dir Memoria: aritmetica1: " + Aritmetica1);
+        System.out.println();
+
+        // este da error, porque espra 2 argumentos, pero haciendo en constructo vacio, ya jala
+        // sobrecarga de constructores
+        var Aritmetica_2 = new Aritmetica3(15,30);
+        // Aritmetica_2.operando1 = 20;
+        // Aritmetica_2.operando2 = 35;
+        Aritmetica_2.sumar();
+
+        // aqui vemos como el oerador this cambia en cada instancia/objeto
+        System.out.println("Dir Memoria: aritmetica2: " + Aritmetica_2);
+
+
+    }
+
 }
